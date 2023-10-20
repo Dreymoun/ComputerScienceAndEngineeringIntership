47da56bcbf2c60c112d16b02a2947e5eae2a2a4c
Dmitriy Falkovskiy <119937239+Dreymoun@users.noreply.github.com>
2023-10-20 16:29:06 +0600

    Add files via upload

---
+++ module6/ConsoleAppModule6/MyLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+
+    public static class PersonInfo
+    {
+        public static string GetInfo(Person person)
+        {
+            return $"{person.FirstName} {person.LastName}, Age: {person.Age}";
+        }
+    }
+}
