85913f0a7446385529b26cdce7d0d77c168a010c
Dmitriy Falkovskiy <119937239+Dreymoun@users.noreply.github.com>
2023-10-20 16:42:51 +0600

    Add files via upload

---
+++ module6/ConsoleApp.Module6.Practice/Bankomat/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    public class Bank
+    {
+        private static int nextAccountId = 1;
+
+        public Account CreateAccount(Client client, decimal initialBalance)
+        {
+            var account = new Account(nextAccountId++, client, initialBalance);
+            client.Account = account;
+            return account;
+        }
+    }
+
+    public class Client
+    {
+        public int Id { get; }
+        public string Password { get; set; }
+        public Account Account { get; set; }
+
+        public Client(int id, string password)
+        {
+            Id = id;
+            Password = password;
+        }
+    }
+
+    public class Account
+    {
+        public int AccountNumber { get; }
+        public Client Owner { get; }
+        public decimal Balance { get; private set; }
+
+        public Account(int accountNumber, Client owner, decimal initialBalance)
+        {
+            AccountNumber = accountNumber;
+            Owner = owner;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public bool TryWithdraw(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ module6/ConsoleApp.Module6.Practice/ConsoleApp.Module6.Practice/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bankomat;
+
+namespace ConsoleApp.Module6.Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            Client client = new Client(1, "password123"); // На практике пароль должен храниться безопасно!
+            bank.CreateAccount(client, 1000); // начальный баланс
+
+            int attempts = 3;
+            while (attempts > 0)
+            {
+                Console.Write("Введите пароль: ");
+                string password = Console.ReadLine();
+                if (password == client.Password) // На практике следует использовать безопасное сравнение!
+                {
+                    ShowMenu(client);
+                    return;
+                }
+                else
+                {
+                    attempts--;
+                    Console.WriteLine($"Неверный пароль. Осталось попыток: {attempts}");
+                }
+            }
+
+            Console.WriteLine("Все попытки ввода пароля исчерпаны.");
+        }
+
+        static void ShowMenu(Client client)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("1. Проверить баланс");
+                Console.WriteLine("2. Пополнить счет");
+                Console.WriteLine("3. Снять деньги");
+                Console.WriteLine("4. Выход");
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine($"Ваш баланс: {client.Account.Balance}");
+                        break;
+                    case "2":
+                        Console.Write("Введите сумму для пополнения: ");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal depositAmount))
+                        {
+                            client.Account.Deposit(depositAmount);
+                            Console.WriteLine($"Счет пополнен на {depositAmount}. Текущий баланс: {client.Account.Balance}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверная сумма.");
+                        }
+                        break;
+                    case "3":
+                        Console.Write("Введите сумму для снятия: ");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal withdrawAmount))
+                        {
+                            if (client.Account.TryWithdraw(withdrawAmount))
+                            {
+                                Console.WriteLine($"Снято {withdrawAmount}. Текущий баланс: {client.Account.Balance}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Недостаточно средств на счету.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверная сумма.");
+                        }
+                        break;
+                    case "4":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Выберите действие из списка.");
+                        break;
+                }
+            }
+        }
+    }
+}
