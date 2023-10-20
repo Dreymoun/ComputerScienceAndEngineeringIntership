a672d4fb48d366dc8618a449ab749628668922da
Dmitriy Falkovskiy <119937239+Dreymoun@users.noreply.github.com>
2023-10-20 16:31:22 +0600

    Add files via upload

---
+++ module6/ConsoleAppModule6.Practice/ConsoleAppModule6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppModule6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLibrary.Person person = new MyLibrary.Person("John", "Doe", 30);
+            string info = MyLibrary.PersonInfo.GetInfo(person);
+
+            Console.WriteLine(info);
+        }
+    }
+}
