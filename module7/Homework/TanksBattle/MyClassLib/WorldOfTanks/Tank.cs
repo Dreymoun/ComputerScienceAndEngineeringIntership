349e7c7c5db17682f84754fde0fa1665b993694a
Dmitriy Falkovskiy <119937239+Dreymoun@users.noreply.github.com>
2023-10-23 22:04:18 +0600

    Add files via upload

---
+++ module7/Homework/TanksBattle/MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        private string _name;
+        private int _ammoLevel;      // Боекомплект
+        private int _armorLevel;     // Уровень брони
+        private int _maneuverabilityLevel;  // Уровень маневренности
+
+        private static Random _random = new Random();
+
+        public Tank(string name)
+        {
+            _name = name;
+            _ammoLevel = _random.Next(101);
+            _armorLevel = _random.Next(101);
+            _maneuverabilityLevel = _random.Next(101);
+        }
+
+        public static Tank operator ^(Tank tank1, Tank tank2)
+        {
+            int winCriteriaTank1 = 0;
+            int winCriteriaTank2 = 0;
+
+            if (tank1._ammoLevel > tank2._ammoLevel) winCriteriaTank1++;
+            else winCriteriaTank2++;
+
+            if (tank1._armorLevel > tank2._armorLevel) winCriteriaTank1++;
+            else winCriteriaTank2++;
+
+            if (tank1._maneuverabilityLevel > tank2._maneuverabilityLevel) winCriteriaTank1++;
+            else winCriteriaTank2++;
+
+            return winCriteriaTank1 > winCriteriaTank2 ? tank1 : tank2;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name}: Ammo Level - {_ammoLevel}, Armor Level - {_armorLevel}, Maneuverability Level - {_maneuverabilityLevel}";
+        }
+    }
+}
---
+++ module7/Homework/TanksBattle/TanksBattle/Program.cs
@@ -0,0 +1,34 @@
+﻿using MyClassLib.WorldOfTanks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanksBattle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] t34Tanks = new Tank[5];
+            Tank[] panteraTanks = new Tank[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                t34Tanks[i] = new Tank("T-34");
+                panteraTanks[i] = new Tank("Pantera");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Battle {i + 1}:");
+                Console.WriteLine(t34Tanks[i]);
+                Console.WriteLine(panteraTanks[i]);
+
+                Tank winner = t34Tanks[i] ^ panteraTanks[i];
+                Console.WriteLine($"Winner: {winner}\n");
+            }
+        }
+    }
+}
