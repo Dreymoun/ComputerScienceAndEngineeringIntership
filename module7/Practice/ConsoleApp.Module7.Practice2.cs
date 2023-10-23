1adb3994dcebe2b01ccfa856d7e03d8b120ded86
Dmitriy Falkovskiy <119937239+Dreymoun@users.noreply.github.com>
2023-10-23 21:05:38 +0600

    Add files via upload

---
+++ module7/Practice/ConsoleApp.Module7.Practice2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Module8.Practice
+{
+    public class ArrayClass
+    {
+        private int[] values;
+
+        public ArrayClass(int[] values)
+        {
+            this.values = values ?? throw new ArgumentNullException(nameof(values));
+        }
+
+        public static bool operator <(ArrayClass left, ArrayClass right)
+        {
+            return left.values.Sum() < right.values.Sum();
+        }
+
+        public static bool operator >(ArrayClass left, ArrayClass right)
+        {
+            return left.values.Sum() > right.values.Sum();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", values);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayClass array1 = new ArrayClass(new int[] { 1, 2, 3 });
+            ArrayClass array2 = new ArrayClass(new int[] { 4, 5, 6 });
+
+            Console.WriteLine($"Array 1: {array1}");
+            Console.WriteLine($"Array 2: {array2}");
+            Console.WriteLine($"Array1 > Array2: {array1 > array2}");
+            Console.WriteLine($"Array1 < Array2: {array1 < array2}");
+        }
+    }
+}
