6bae32234f917e152a1727bf132bcc3132ce8084
Dmitriy Falkovskiy <119937239+Dreymoun@users.noreply.github.com>
2023-11-11 21:35:56 +0600

    Add files via upload

---
+++ module8/Homework/Module8.Homework.Task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8.Practice
+{
+    public class SquaredArray
+    {
+        private int[] _array;
+
+        public SquaredArray(int size)
+        {
+            _array = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get => _array[index];
+            set => _array[index] = value * value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var squaredArray = new SquaredArray(3);
+            squaredArray[0] = 2;
+            squaredArray[1] = 3;
+            squaredArray[2] = 4;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"squaredArray[{i}] = {squaredArray[i]}");
+            }
+        }
+    }
+
+}
