d2c7d8600a033c2a3b14ea82660536dd939e0799
Dmitriy Falkovskiy <119937239+Dreymoun@users.noreply.github.com>
2023-11-24 20:32:16 +0600

    Add files via upload

---
+++ module12/Module12.Practice.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12.Practice
+{
+    public interface iPropertychanged
+    {
+        event PropertyeventHandler Propertychanged;
+    }
+
+    public delegate void PropertyeventHandler(object sender, PropertyeventArgs e);
+
+    public class PropertyeventArgs : EventArgs
+    {
+        public string PropertyName { get; private set; }
+
+        public PropertyeventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+
+    public class MyClass : iPropertychanged
+    {
+        private int myProperty;
+        public event PropertyeventHandler Propertychanged;
+
+        public int MyProperty
+        {
+            get { return myProperty; }
+            set
+            {
+                if (myProperty != value)
+                {
+                    myProperty = value;
+                    OnPropertyChanged(nameof(MyProperty));
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            Propertychanged?.Invoke(this, new PropertyeventArgs(propertyName));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myObj = new MyClass();
+            myObj.Propertychanged += MyObj_Propertychanged;
+
+            myObj.MyProperty = 10; // Изменение свойства
+            myObj.MyProperty = 20; // Еще одно изменение свойства
+        }
+
+        private static void MyObj_Propertychanged(object sender, PropertyeventArgs e)
+        {
+            Console.WriteLine($"Property changed: {e.PropertyName}");
+        }
+    }
+}
